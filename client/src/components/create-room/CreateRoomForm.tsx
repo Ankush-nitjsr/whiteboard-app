1099f14b80b520a297631c1ca627b6da14d66bf0
Ankush <ankushnitjsr@gmail.com>
2025-01-08 23:25:44 +0530

    feat: Add CreateRoomForm component
    To handle creation of new room

---
+++ client/src/components/create-room/CreateRoomForm.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Socket } from "socket.io-client";
+
+interface CreateRoomFormProps {
+  uuid: () => string;
+  socket: Socket;
+  setUser: (user: string) => void;
+}
+
+const CreateRoomForm = ({ uuid, socket, setUser }: CreateRoomFormProps) => {
+  const [name, setName] = useState("");
+  const [roomId, setRoomId] = useState(uuid());
+
+  const handleCreateRoom = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const roomData = {
+      name,
+      roomId,
+      userId: uuid(),
+      host: true,
+      presenter: true,
+    };
+
+    setUser(name);
+    socket.emit("userJoined", roomData);
+  };
+
+  const handleCopyRoomId = () => {
+    navigator.clipboard.writeText(roomId).then(
+      () => alert("Room ID copied to clipboard!"),
+      () => alert("Failed to copy Room ID.")
+    );
+  };
+
+  return (
+    <form
+      className="flex flex-col w-[35%] p-4 space-y-4 border border-blue-500"
+      onSubmit={handleCreateRoom}
+    >
+      <h1 className="text-xl font-bold text-blue-700">Create Room</h1>
+      <input
+        type="text"
+        className="w-full px-2 py-1"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <div className="flex space-x-2">
+        <input
+          type="text"
+          value={roomId}
+          className="p-1 w-full"
+          disabled
+          placeholder="Room code"
+        />
+        <button
+          className="border border-blue-500"
+          onClick={() => setRoomId(uuid())}
+        >
+          Generate
+        </button>
+        <button
+          type="button"
+          className="border border-blue-500"
+          onClick={handleCopyRoomId}
+        >
+          Copy
+        </button>
+      </div>
+      <button type="submit" className="border border-blue-500">
+        Generate Room
+      </button>
+    </form>
+  );
+};
+
+export default CreateRoomForm;
