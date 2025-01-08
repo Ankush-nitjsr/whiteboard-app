a6f567d28ec75078a8b7b9fcaa2d5df444682c33
Ankush <ankushnitjsr@gmail.com>
2025-01-08 23:23:31 +0530

    feat: Add CanvasDrawing component for real-time drawing functionality

---
+++ client/src/components/canvas-drawing/CanvasDrawing.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import Board from "../board/Board";
+
+const CanvasDrawing = () => {
+  const [brushColor, setBrushColor] = useState<string>("black");
+  const [brushSize, setBrushSize] = useState<number>(5);
+
+  useEffect(() => {
+    console.log("CanvasDrawing Brush Size:", brushSize);
+  }, [brushSize]);
+
+  return (
+    <div className="App">
+      <h1>Collaborative Whiteboard</h1>
+      <div>
+        <Board brushColor={brushColor} brushSize={brushSize} />
+        <div className="tools">
+          <div>
+            <label>
+              <span>Color: </span>
+              <input
+                type="color"
+                value={brushColor}
+                onChange={(e) => setBrushColor(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <span>Size: </span>
+              <input
+                type="range"
+                min="1"
+                max="100"
+                value={brushSize}
+                onChange={(e) => setBrushSize(Number(e.target.value))}
+              />
+            </label>
+            <span>{brushSize}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CanvasDrawing;
