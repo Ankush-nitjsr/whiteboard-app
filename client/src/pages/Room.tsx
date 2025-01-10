cddd4d0d934c1279c46ad1bd252b7bb4e2fc7d2e
Ankush <ankushnitjsr@gmail.com>
2025-01-10 19:00:38 +0530

    chore: Update to remove type errors and enhance UI

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,18 +1,24 @@
 import CreateRoomForm from "../components/create-room/CreateRoomForm";
 import { JoinRoomForm } from "../components/join-room/JoinRoomForm";
 import { Socket } from "socket.io-client";
+import User from "../../types/user";
 
 interface RoomProps {
   uuid: () => string;
   socket: Socket;
-  setUser: React.Dispatch<React.SetStateAction<string | null>>;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
 }
 
 export const Room = ({ uuid, socket, setUser }: RoomProps) => {
   return (
-    <div className="flex justify-evenly gap-4 w-full">
-      <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser} />
-      <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
-    </div>
+    <>
+      <h1 className="font-bold text-3xl mt-10 text-blue-600">
+        Collaborative Whiteboard
+      </h1>
+      <div className="flex justify-evenly w-full mt-16">
+        <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser} />
+        <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
+      </div>
+    </>
   );
 };
