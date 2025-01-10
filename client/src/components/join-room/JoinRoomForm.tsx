b45acc2f10c3e7e7c16874f72c072c198786f2b5
Ankush <ankushnitjsr@gmail.com>
2025-01-10 21:58:12 +0530

    chore: Add id, name and autocomplete attributes

--- client/src/components/join-room/JoinRoomForm.tsx
+++ client/src/components/join-room/JoinRoomForm.tsx
@@ -44,23 +44,33 @@ export const JoinRoomForm = ({ uuid, socket, setUser }: JoinRoomFormProps) => {
 
       {/* Name Input */}
       <div>
-        <label className="block text-gray-700 font-medium mb-2">
+        <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
           Your Name
         </label>
         <input
+          id="name"
+          name="name"
           type="text"
           className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Enter your name"
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
+          autoComplete="on"
         />
       </div>
 
       {/* Room ID Input */}
       <div>
-        <label className="block text-gray-700 font-medium mb-2">Room ID</label>
+        <label
+          htmlFor="roomId"
+          className="block text-gray-700 font-medium mb-2"
+        >
+          Room ID
+        </label>
         <input
+          id="roomId"
+          name="roomId"
           type="text"
           className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Enter Room ID"
