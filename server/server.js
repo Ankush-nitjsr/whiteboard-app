eda9d37ebb887a6a434ff49556eeccda0f78fbc0
Ankush <ankushnitjsr@gmail.com>
2025-01-10 17:38:41 +0530

    chore: Removed typo error

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: "http://localhost:5173",q
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
