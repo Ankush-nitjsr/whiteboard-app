fb047dbaca0c102c1e92a962fcee2d4198697395
Ankush <ankushnitjsr@gmail.com>
2025-01-10 19:01:14 +0530

    chore: Update to remove type errors and enhance UI

--- client/src/components/canvas-drawing/CanvasDrawing.tsx
+++ client/src/components/canvas-drawing/CanvasDrawing.tsx
@@ -1,24 +1,27 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import "./style.css";
-import Board from "../board/Board";
 import { NewBoard } from "../board/NewBoard";
+import { Socket } from "socket.io-client";
+import User from "../../../types/user";
+import { Users } from "../../../types/users";
+import Element from "../../../types/element";
 
-const CanvasDrawing = ({ user, socket, users }) => {
+interface CanvasDrawingProps {
+  user: User;
+  socket: Socket;
+  users: Users;
+}
+
+const CanvasDrawing = ({ user, socket, users }: CanvasDrawingProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const ctxRef = useRef(null);
 
-  console.log("User", user);
-
   const [tool, setTool] = useState("pencil");
   const [brushColor, setBrushColor] = useState<string>("#000000");
-  const [brushSize, setBrushSize] = useState<number>(5);
-  const [elements, setElements] = useState([]);
-  const [history, setHistory] = useState([]);
-  const [openedUserTab, setOpenedUserTab] = useState(false);
-
-  useEffect(() => {
-    console.log("CanvasDrawing Brush Size:", brushSize);
-  }, [brushSize]);
+  const [brushSize, setBrushSize] = useState<number>(2);
+  const [elements, setElements] = useState<Element[]>([]);
+  const [history, setHistory] = useState<Element[]>([]);
+  const [openedUserTab, setOpenedUserTab] = useState<boolean>(false);
 
   const handleClearCanvas = () => {
     const canvas = canvasRef.current;
@@ -33,7 +36,7 @@ const CanvasDrawing = ({ user, socket, users }) => {
   };
 
   const undo = () => {
-    setHistory((prevHistory) => [
+    setHistory((prevHistory: Element[]) => [
       ...prevHistory,
       elements[elements.length - 1],
     ]);
@@ -53,11 +56,14 @@ const CanvasDrawing = ({ user, socket, users }) => {
   return (
     <>
       <button
-        className="bg-black text-white absolute top-[12%] left-[5%] w-32 h-10"
+        className="fixed top-[12%] left-[5%] flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white font-normal rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300 w-36 h-18 py-2"
         onClick={() => setOpenedUserTab(true)}
       >
-        Users
+        <span className="text-base">Users Online</span>
+        <span className="text-sm">{users.length}</span>
+        <span className="text-xs italic opacity-80">Click to expand</span>
       </button>
+
       {openedUserTab && (
         <div className="fixed top-0 h-[100vh] text-white bg-black w-[250px] left-0 p-4">
           <button
@@ -69,100 +75,115 @@ const CanvasDrawing = ({ user, socket, users }) => {
           <div className="mt-5 w-[100%] pt-5">
             {users.map((usr, idx) => (
               <p key={idx * 999} className="my-2 w-[100%] text-center">
-                {usr.name}
-                {user && user.userId === usr.userId && "(You)"}
+                {usr.name} {user && user.userId === usr.userid && "( You)"}
               </p>
             ))}
           </div>
         </div>
       )}
       <div className="canvas-drawing">
-        <h1>
-          Collaborative Whiteboard <span>[Users Online : {users.length}]</span>
-        </h1>
-        {user && user.presenter && (
-          <div className="tools">
-            <div>
-              <label>
-                <span>Pencil: </span>
-                <input
-                  type="radio"
-                  value="pencil"
-                  checked={tool === "pencil"}
-                  onChange={(e) => setTool(e.target.value)}
-                />
-              </label>
+        <h1 className="font-bold text-3xl">Collaborative Whiteboard</h1>
+        <div>
+          {user && user.presenter && (
+            <div className="tools">
+              <div className="flex space-x-2">
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      value="pencil"
+                      checked={tool === "pencil"}
+                      onChange={(e) => setTool(e.target.value)}
+                    />
+                    <span className="text-sm">Pencil</span>
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      value="line"
+                      checked={tool === "line"}
+                      onChange={(e) => setTool(e.target.value)}
+                    />
+                    <span className="text-sm">Line</span>
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      value="rectangle"
+                      checked={tool === "rectangle"}
+                      onChange={(e) => setTool(e.target.value)}
+                    />
+                    <span className="text-sm">Rectangle</span>
+                  </label>
+                </div>
+              </div>
+              <div>
+                <label className="flex justify-center items-center">
+                  <span className="text-sm">Color: </span>
+                  <input
+                    type="color"
+                    value={brushColor}
+                    onChange={(e) => setBrushColor(e.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="flex justify-center items-center">
+                <label className="flex justify-center items-center">
+                  <span className="text-sm">Size: </span>
+                  <input
+                    type="range"
+                    min="1"
+                    max="100"
+                    value={brushSize}
+                    onChange={(e) => setBrushSize(Number(e.target.value))}
+                  />
+                </label>
+                <span>{brushSize}</span>
+              </div>
+              <div className="space-x-2">
+                <button
+                  disabled={elements.length === 0}
+                  onClick={undo}
+                  className="text-sm bg-gradient-to-r from-blue-500 to-purple-600 text-white px-2 py-1 rounded-lg"
+                >
+                  Undo
+                </button>
+                <button
+                  disabled={history.length < 1}
+                  onClick={redo}
+                  className="text-sm bg-gradient-to-r from-blue-500 to-purple-600 text-white px-2 py-1 rounded-lg"
+                >
+                  Redo
+                </button>
+              </div>
+              <div>
+                <button
+                  onClick={handleClearCanvas}
+                  className="text-sm bg-red-400 px-2 py-1 rounded-lg"
+                >
+                  Clear Canvas
+                </button>
+              </div>
             </div>
-            <div>
-              <label>
-                <span>Line: </span>
-                <input
-                  type="radio"
-                  value="line"
-                  checked={tool === "line"}
-                  onChange={(e) => setTool(e.target.value)}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <span>Rectangle: </span>
-                <input
-                  type="radio"
-                  value="rectangle"
-                  checked={tool === "rectangle"}
-                  onChange={(e) => setTool(e.target.value)}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <span>Color: </span>
-                <input
-                  type="color"
-                  value={brushColor}
-                  onChange={(e) => setBrushColor(e.target.value)}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                <span>Size: </span>
-                <input
-                  type="range"
-                  min="1"
-                  max="100"
-                  value={brushSize}
-                  onChange={(e) => setBrushSize(Number(e.target.value))}
-                />
-              </label>
-              <span>{brushSize}</span>
-            </div>
-            <div className="space-x-2">
-              <button disabled={elements.length === 0} onClick={undo}>
-                Undo
-              </button>
-              <button disabled={history.length < 1} onClick={redo}>
-                Redo
-              </button>
-            </div>
-            <div>
-              <button onClick={handleClearCanvas}>Clear Canvas</button>
-            </div>
-          </div>
-        )}
+          )}
 
-        {/* <Board brushColor={brushColor} brushSize={brushSize} /> */}
-        <NewBoard
-          canvasRef={canvasRef}
-          ctxRef={ctxRef}
-          elements={elements}
-          setElements={setElements}
-          tool={tool}
-          color={brushColor}
-          user={user}
-          socket={socket}
-        />
+          {/* <Board brushColor={brushColor} brushSize={brushSize} /> */}
+          <NewBoard
+            canvasRef={canvasRef}
+            ctxRef={ctxRef}
+            elements={elements}
+            setElements={setElements}
+            tool={tool}
+            color={brushColor}
+            brushSize={brushSize}
+            user={user}
+            socket={socket}
+          />
+        </div>
       </div>
     </>
   );
