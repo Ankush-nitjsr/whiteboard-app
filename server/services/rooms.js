6e02fb031a7ca0707035291844eaf82ed52bf015
Ankush <ankushnitjsr@gmail.com>
2025-01-13 09:09:19 +0530

    feat: Add error handling & logging and update query command in rooms.js (services)
    - Wrap all functions in try-catch blocks to improve error handling.
    - Add log statements for easier debugging and traceability.
    - Update database query command
    - Add getRoom function to get room by roomId

--- server/services/rooms.js
+++ server/services/rooms.js
@@ -4,29 +4,49 @@ const db = require("./db");
  * Fetches all rooms.
  */
 async function getRooms() {
-  const rows = await db.query(
-    `SELECT id, roomid, hostid, created_at 
-     FROM rooms`
-  );
+  try {
+    const rooms = await db.query("SELECT * FROM rooms");
+    return rooms;
+  } catch (error) {
+    console.error("Error fetching rooms:", error);
+    throw error;
+  }
+}
 
-  return rows;
+/**
+ * Fetches room with specific roomId.
+ */
+async function getRoom(roomId) {
+  try {
+    const room = await db.query("SELECT * FROM rooms WHERE roomid = $1", [
+      roomId,
+    ]);
+    return room[0];
+  } catch (error) {
+    console.error("Error fetching room:", error);
+    throw error;
+  }
 }
 
 /**
  * Creates a new room.
  */
-async function createRoom(room) {
-  const { roomId, hostId } = room;
-  const result = await db.query(
-    `INSERT INTO rooms (roomid, hostid) 
-     VALUES ($1, $2) RETURNING id`,
-    [roomId, hostId]
-  );
-
-  return { id: result[0].id }; // PostgreSQL returns rows as an array
+async function createRoom(roomData) {
+  const { roomId, hostId } = roomData;
+  try {
+    const newRoom = await db.query(
+      "INSERT INTO rooms (roomid, hostid) VALUES ($1, $2) RETURNING *",
+      [roomId, hostId]
+    );
+    return newRoom[0];
+  } catch (error) {
+    console.error("Error creating room:", error);
+    throw error;
+  }
 }
 
 module.exports = {
   getRooms,
+  getRoom,
   createRoom,
 };
