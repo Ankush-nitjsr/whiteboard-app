8016f075c1859b23e5683c091b78d532fd588f9b
Ankush <ankushnitjsr@gmail.com>
2025-01-13 09:40:24 +0530

    style: Update Rooms component
    Add style for responsiveness across all devices with mobile first approach.

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -12,10 +12,10 @@ interface RoomProps {
 export const Room = ({ uuid, socket, setUser }: RoomProps) => {
   return (
     <>
-      <h1 className="font-bold text-3xl mt-10 text-blue-600">
+      <h1 className="font-bold text-3xl mt-10 text-blue-600 text-center">
         Collaborative Whiteboard
       </h1>
-      <div className="flex justify-evenly w-full mt-16">
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-evenly w-full mt-8 mb-8 md:mt-16 space-y-8 lg:space-y-0">
         <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser} />
         <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
       </div>
